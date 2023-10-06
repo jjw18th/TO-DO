ef108a4db57da506c7aa5ce13cf9a61911b9182c
Darshita Jhunjhunwala <darshu18th@gmail.com>
2023-10-06 14:21:52 +0530

    made changes in the svg icon

--- script.js
+++
@@ -1,6 +0,0 @@
-const arr=[];
-document.getElementById("addTask").addEventListener("click",()=>{
-    const text=document.querySelector("#input-task").value;
-    //console.log(text);
-    arr.push(text);
-});
